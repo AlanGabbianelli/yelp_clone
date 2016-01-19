ec8fdb9d0f55dd16b135357fe8970d378f798b95
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-19 11:42:31 +0000

    Adds Facebook like and share buttons in all restaurants

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '207993559544395',
+    xfbml      : true,
+    version    : 'v2.5'
+  });
+};
+
+(function(d, s, id){
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
+ }(document, 'script', 'facebook-jssdk'));
